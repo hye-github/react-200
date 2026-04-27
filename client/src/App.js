812faba343c3e9b90f390b9e467f4a26f4057107
hye <hezuc12@gmail.com>
2026-04-27 22:17:59 +0900

    012

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import SpeacialOperator from './R011_SpeacialOperator'
+import ClassPrototype from './R012_ClassPrototype'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <SpeacialOperator/>
+      <ClassPrototype/>
     </div>
   );
 }
