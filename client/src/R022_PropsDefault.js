b152b7b02ece66438dd4e472c77d4e20b31fc335
hye <hezuc12@gmail.com>
2026-05-08 23:37:48 +0900

    022

--- client/src/R022_PropsDefault.js
+++ client/src/R022_PropsDefault.js
@@ -8,8 +8,7 @@ class R022_PropsDefault extends Component {
         } = this.props // 지역변수 선언 후 props로 전달된 값 할당
         return ( 
             <div style={{padding: "0px"}}>
-                {ReactString}{ReactNumber} 
-                {/*상위 컴포넌트에서는 ReactString의 값을 전달하지않음. 원래는 콘솔창에서 에러나야하는데 상위 버전이라 에러가 나지않음.*/}
+                {ReactString}{ReactNumber}
             </div>
         )
     }
@@ -19,7 +18,7 @@ class R022_PropsDefault extends Component {
 // defaultProps 라는 문법 사용
 R022_PropsDefault.defaultProps = {
     ReactString: "리액트",
-    ReactNumber: 400
+    ReactNumber: 400 //부모 컴포넌트의 값인 200 사용됨
 }
 
 export default R022_PropsDefault;
